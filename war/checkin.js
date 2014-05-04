6c658061de9d009315e62b36f5f53d98268dc05f
Alline Oliveira <alline.oliveira@gmail.com>
2014-05-03 15:25:46 -1000

    Update checkin.js

--- war/checkin.js
+++ war/checkin.js
@@ -1,13 +1,32 @@
-$(document).on("pageinit", function(event) {
-alert('test');
+function addCheckinMarkers() {
+
 	var getCheckinUrl = "test";
 
-	$.post(getCheckinUrl, {}, function(data, status) {
-		alert(data);
-		restaurantList = jQuery.parseJSON(data);
-		
-		alert(restaurantList);
+	$
+			.post(
+					getCheckinUrl,
+					{},
+					function(data, status) {
 
-	});
+						restaurantList = jQuery.parseJSON(data);
 
-});
+						$
+								.each(
+										restaurantList,
+										function(key, val) {
+
+											var point = new google.maps.LatLng(
+													val.latitude, val.longitude);
+
+											var marker = new google.maps.Marker(
+													{
+														map : map,
+														position : point,
+														title : val.restaurantName,
+														icon : "http://labs.google.com/ridefinder/images/mm_20_blue.png"
+													});
+
+										});
+
+					});
+}
